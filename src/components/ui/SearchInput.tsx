d313802479aa87361ece94a7d1790cbeaec9ef97
BR-BelalRezk <belalrezk.developer@gmail.com>
2024-10-03 16:07:29 +0300

    push

--- src/components/ui/SearchInput.tsx
+++ src/components/ui/SearchInput.tsx
@@ -1,24 +1,52 @@
 "use client";
 import { useProducts } from "@/context/ProductContextProvider";
+import { useEffect, useState } from "react";
 import { IoSearch } from "react-icons/io5";
+import { motion } from "framer-motion";
 
 export default function SearchInput({ className }: { className: string }) {
   const { setSearchQuery, searchQuery } = useProducts();
+  const [searchValue, setSearchValue] = useState("");
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setSearchQuery(searchValue);
   };
+  useEffect(() => {
+    if (!searchValue) {
+      setSearchQuery("");
+    }
+  }, [searchValue]);
   return (
     <form onSubmit={handleSubmit} className=" relative w-full">
       <input
-        value={searchQuery}
+        value={searchValue}
         type="search"
-        onChange={(e) => setSearchQuery(e.target.value)}
+        onChange={(e) => setSearchValue(e.target.value)}
         placeholder="search"
         className={`${className} relative z-40`}
       />
-      <button className=" absolute right-5 block z-50 top-[15%] text-3xl">
+      <motion.button
+        initial={{ scale: 1 }}
+        whileTap={{ scale: 0.8, color: "#E5E5E5" }}
+        animate={
+          !searchValue
+            ? {
+                scale: [1.2, 1, 1.2],
+                transition: {
+                  type: "tween",
+                  ease: "linear",
+                  repeat: Infinity,
+                  repeatType: "loop",
+                  duration: 1.5,
+                },
+              }
+            : {}
+        }
+        type="submit"
+        className="absolute right-5 block z-50 top-[20%] text-3xl text-main"
+      >
         <IoSearch />
-      </button>
+      </motion.button>
     </form>
   );
 }
